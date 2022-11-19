d58d819c8da7cfcda515ba6024e3518fb0abf3ed
tal0311 <tal.amit0311@gmail.com>
2022-11-19 14:58:57 +0200

    rooms msg

--- services/socket.service.js
+++ services/socket.service.js
@@ -7,7 +7,18 @@ function socketService(server) {
 
     socket.on('chat-msg', (msg) => {
       console.log(msg)
+      if (socket.topic) {
+        io.in(socket.topic).emit('new-msg', msg)
+        return
+      }
       io.emit('new-msg', msg)
+    })
+
+    socket.on('chat-topic', (topic) => {
+      if (socket.topic) socket.topic = null
+      socket.topic = topic
+      console.log('socket:', socket)
+      socket.join(topic)
     })
     socket.on('disconnect', () => {
       console.log('user disconnected')
